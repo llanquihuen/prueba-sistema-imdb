71775a4dc9e2ee35803194fed4730aed6db26af7
Luis Ramirez <llanquihuen@gmail.com>
2021-09-24 01:04:10 -0300

    procfile2

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/*.jar
+
--- src/main/java/cl/desafiolatam/PruebaSistemaImDbApplication.java
+++ src/main/java/cl/desafiolatam/PruebaSistemaImDbApplication.java
@@ -2,10 +2,11 @@ package cl.desafiolatam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class PruebaSistemaImDbApplication {
+public class PruebaSistemaImDbApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaSistemaImDbApplication.class, args);
